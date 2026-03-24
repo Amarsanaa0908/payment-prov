789a614f94af0ddc09183393be34871adc24b77e
Amarsanaa0908 <walleolle8@gmail.com>
2026-03-24 22:10:27 +0800

    done

--- src/pages/merchant_2/[slug]/[id]/index.js
+++ src/pages/merchant_2/[slug]/[id]/index.js
@@ -208,7 +208,7 @@ useEffect(() => {
                       type="tel"
                       name="phone"
                       placeholder="Утасны дугаар"
-                      {...register('phone')}
+                      {...register('phone', {required:true})}
                       className="px-4 py-3 border border-gray-300 rounded-md focus:ring-2 focus:ring-blue-500 focus:border-transparent text-black"
                       required
                     />
