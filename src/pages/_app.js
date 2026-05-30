275b8d62882b160703428f864b563414c898d51f
Amarsanaa0908 <walleolle8@gmail.com>
2026-05-30 15:30:28 +0800

    dahindurla

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -22,6 +22,7 @@ export default function App({ Component, pageProps }) {
     "/merchant_4/[slug]/[id]",
     "/merchant_5/[slug]/[id]",
     "/merchant_6/[slug]/[id]",
+    "/merchant_7/[slug]/[id]",
     "/",
     "/about",
     "/contact",
